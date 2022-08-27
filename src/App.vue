32af5a8917a6f1bcc6e77d514cc83845623ee11f
color <color.yan@gzgi.com>
2022-08-27 23:12:31 +0800

    build: init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { onErrorCaptured, onUpdated, ref, computed } from 'vue'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+const error = ref()
+
+onErrorCaptured((innerError, instance, info) => {
+  console.error(innerError.message, info)
+  error.value = innerError
+  return false
+})
+onUpdated(() => {
+  console.info({error}, error.message)
+})
+
+const errorMessage = computed(() => {
+  return error.value && error.value.message
+})
+
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld v-if="!errorMessage" msg="Vite + Color" />
+  <HelloWorld2 v-if="!errorMessage" msg="Helloworld2" />
+  <h2 v-else>{{errorMessage}}</h2>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+
+<script setup>
+import { ref, onMounted, onUpdated, watch } from 'vue'
+
+import { Button } from 'ant-design-vue';
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+const bool = ref(false)
+
+onMounted(() => {
+  console.log(`the component is now mounted.`)
+})
+
+watch(count, () => {
+  // 文本内容应该与当前的 `count.value` 一致
+  console.log('watch:', count.value)
+  if (count.value > 4) {
+    throw new Error('fuck , go to shit')
+  }
+})
+
+const onClick = (e) => {
+  count.value += 1
+}
+
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+  <div class="card">
+    <a-button @click="onClick">count is {{ count }}</a-button>
+    <p>
+      Edit
+      <code>components/HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
