925aa27b9382ae32099f850dbb9b2844d11ef1c9
color <color.yan@gzgi.com>
2022-08-31 23:51:07 +0800

    update

---
+++ src/pages/Page3/index.vue
@@ -0,0 +1,5 @@
+<script setup></script>
+
+<template>
+    <h1 v-for="i in 100" :key="i">Page3</h1>
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { ref, defineAsyncComponent  } from 'vue'
+    const Page1 = defineAsyncComponent (() => import(/* webpackChunkName: "tabs-page1" */ './Page1/index.vue'))
+    const Page2 = defineAsyncComponent (() => import(/* webpackChunkName: "tabs-page2" */ './Page2/index.vue'))
+    const Page3 = defineAsyncComponent (() => import(/* webpackChunkName: "tabs-page3" */ './Page3/index.vue'))
+
+    const activeKey = ref('3')
+
+</script>
+
+<template>
+<div>
+    <a-tabs v-model:activeKey="activeKey" centered  :tabBarGutter="100">
+      <a-tab-pane key="1" tab="Tab 1" style="height: 200px"><Page1/></a-tab-pane>
+      <a-tab-pane key="2" tab="Tab 2" force-render><Page2/></a-tab-pane>
+      <a-tab-pane key="3" tab="Tab 3"><Page3 /></a-tab-pane>
+    </a-tabs>
+</div>
+</template>
+
+<style scoped>
+.page {
+    height: 100vh;
+}
+.ant-tabs {
+    height: 100%;
+}
+
+:deep(.ant-tabs-content-holder) {
+    height: 100vh;
+    overflow-y: scroll;
+}
+</style>
