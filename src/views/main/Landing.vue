fc0518f2214a650d7cd76383ab6f317fd9e371dd
yellowgocl <yellowgod@live.cn>
2023-12-27 13:39:12 +0800

    feat

--- src/views/main/Landing.vue
+++ src/views/main/Landing.vue
@@ -4,8 +4,14 @@ const { t, locale } = useI18n({ useScope: 'global' })
 </script>
 <template>
     <common-page slim>
-        <div md:block>
-            <img src="@/assets/images/landing-bg.png" w-full alt="login_banner" />
+        <div >
+            <div relative>
+                <article class="absolute top-20 left-8 right-40">
+                    <img src="@/assets/images/logo-lg.png" class='w-[120px] block mb-4' alt="login_banner" />
+                    <span whitespace-pre-line v-t="'landing.page-1.descript'"></span>
+                </article>
+                <img src="@/assets/images/landing-bg.png" w-full alt="login_banner" />
+            </div>
         </div>
     </common-page>
 </template>
